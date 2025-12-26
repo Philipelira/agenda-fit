afac87e7b1f0ad166fb94cbefa0f7605c98af995
Joao Cordeiro <joaofreitas102003@gmail.com>
2025-12-26 20:18:54 -0300

    Sprint 7 implementação de senha

--- js/alunos.js
+++ js/alunos.js
@@ -4,6 +4,7 @@ function cadastrarAluno() {
   const nome = document.getElementById('nome').value;
   const telefone = document.getElementById('telefone').value;
   const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
 
   fetch(API, {
   method: 'POST',
